ac9a9887432c2f882b220c5e83fac91a5a4a6988
Bruno Henrique <brunowhenrique1@gmail.com>
2026-09-09 23:17:09 -0300

    Nova classe de Token e atualização na autenticacaoController

--- src/main/java/br/com/forum_hub/controller/AutentificacaoController.java
+++ src/main/java/br/com/forum_hub/controller/AutentificacaoController.java
@@ -1,6 +1,8 @@
 package br.com.forum_hub.controller;
 
 import br.com.forum_hub.domain.autenticacao.DadosLogin;
+import br.com.forum_hub.domain.autenticacao.TokenService;
+import br.com.forum_hub.domain.usuario.Usuario;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,15 +17,19 @@ public class AutentificacaoController {
 
 
     private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
-    public AutentificacaoController(AuthenticationManager authenticationManager) {
+    public AutentificacaoController(AuthenticationManager authenticationManager, TokenService tokenService) {
         this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Authentication> efetuarLogin(@Valid @RequestBody DadosLogin dados){
+    public ResponseEntity<String> efetuarLogin(@Valid @RequestBody DadosLogin dados){
         var autenticationToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
         var authentication = authenticationManager. authenticate(autenticationToken);
-        return ResponseEntity.ok(authentication);
+
+        String tokenAcesso = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(tokenAcesso);
     }
 }
