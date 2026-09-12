19dd87ddf30904812922ab7b2c136179aa64c027
Bruno Henrique <brunowhenrique1@gmail.com>
2026-09-12 19:07:25 -0300

    Crianco filtro de acesso do token e modificando TokenService e config

---
+++ src/main/java/br/com/forum_hub/infra/seguranca/FiltroTokenAcesso.java
@@ -0,0 +1,34 @@
+package br.com.forum_hub.infra.seguranca;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class FiltroTokenAcesso extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //Recuperar o token da requisição -> parametro request
+        String token = recuperarTokenRequisicao(request); //usamos o request
+
+        if(token != null){
+            //validacao do token
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarTokenRequisicao(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader != null){
+            return authorizationHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
+}
