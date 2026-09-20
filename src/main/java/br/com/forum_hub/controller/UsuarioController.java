de9ea8b851052e4e1bc6f7641fb9af5c895d8ebc
Bruno Henrique <brunowhenrique1@gmail.com>
2026-09-20 20:49:45 -0300

    Verificando endereço email do Usuário com token

--- src/main/java/br/com/forum_hub/controller/UsuarioController.java
+++ src/main/java/br/com/forum_hub/controller/UsuarioController.java
@@ -9,9 +9,7 @@ import br.com.forum_hub.domain.usuario.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -28,5 +26,12 @@ public class UsuarioController {
         var usuario = usuarioService.cadastrar(dados);
         var uri = uriBuilder.path("/{nomeUsuario}").buildAndExpand(usuario.getNomeUsuario()).toUri();
         return ResponseEntity.created(uri).body(new DadosListagemUsuario(usuario));
+    }
+
+    @GetMapping("/verificar-conta")
+    public ResponseEntity<String> verificarEmail(@RequestParam String codigo){
+        usuarioService.verificarEmail(codigo);
+        return ResponseEntity.ok("Conta verificada com sucesso!");
+
     }
 }
